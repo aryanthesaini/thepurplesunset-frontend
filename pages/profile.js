abf990c405523b74ae03f5fc6e473fc3e07b194b
aryanthesaini <sainiaryan2222@gmail.com>
2022-10-24 00:42:08 +0530

    attempt 1 key was the reason deployment failed

--- pages/profile.js
+++ pages/profile.js
@@ -26,7 +26,7 @@ export default function Profile({ user, orders }) {
         <p>{user.email}</p>
         <div>
           {orders.map((order) => (
-            <Order>
+            <Order key={order.id}>
               <h1>Order Number: {order.id}</h1>
               <h2>Amount: ₹ {order.amount / 100}</h2>
               <h2>Receipt Email: {user.email}</h2>
