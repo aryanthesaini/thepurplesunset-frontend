2a1d1cf50d9bf42a420112cf4036f1bb8640e43b
aryanthesaini <sainiaryan2222@gmail.com>
2022-11-11 21:48:35 +0530

    landing page bug fixed

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,38 +1,50 @@
 import { PageContainer, Heading, ButtonDiv } from '../styles/LandingPage';
 import Typewriter from 'typewriter-effect';
+import { useStateContext } from '../lib/context';
 
 export default function LandingPage() {
+  const { showItems, setShowItems } = useStateContext();
+  const handleClick = () => {
+    setShowItems(false);
+  };
   return (
-    <PageContainer>
-      <Heading>
-        <h1>THE PURPLE SUNSET</h1>
-        <h3>By Gunveen Kaur</h3>
-        <h2>
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter
+    showItems && (
+      <PageContainer>
+        <Heading>
+          <h1>THE PURPLE SUNSET</h1>
+          <h3>By Gunveen Kaur</h3>
+          <h2>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
 
-                .typeString('GET THEM YOUR CUSTOMISED ')
-                .pauseFor(300)
+                  .typeString('GET THEM YOUR CUSTOMISED ')
+                  .pauseFor(300)
 
-                .typeString('LETTER')
-                .deleteChars(6)
-                .typeString('PHOTO FRAME')
-                .deleteChars(11)
-                .typeString('CUP')
-                .deleteChars(3)
-                .typeString('SPOTIFY PLAGUE')
-                .deleteChars(14)
-                .typeString('LOVE')
-                .pauseFor(1000)
-                .start();
-            }}
-          />
-        </h2>
-      </Heading>
-      <ButtonDiv>
-        <button onClick={() => window.scrollBy(0, 700)}>GO SHOPPING</button>
-      </ButtonDiv>
-    </PageContainer>
+                  .typeString('LETTER')
+                  .deleteChars(6)
+                  .typeString('PHOTO FRAME')
+                  .deleteChars(11)
+                  .typeString('CUP')
+                  .deleteChars(3)
+                  .typeString('SPOTIFY PLAGUE')
+                  .deleteChars(14)
+                  .typeString('LOVE')
+                  .pauseFor(1000)
+                  .start();
+              }}
+            />
+          </h2>
+        </Heading>
+        <ButtonDiv>
+          <button
+            onClick={() => {
+              handleClick();
+            }}>
+            GO SHOPPING
+          </button>
+        </ButtonDiv>
+      </PageContainer>
+    )
   );
 }
